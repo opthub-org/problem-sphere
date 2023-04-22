941dae55ded7d5c5fb4a0d7218e8edf0c5fb3525
Naoki Hamada <hmkz@s101.xrea.com>
2023-04-22 09:19:05 +0000

    refactor: fix pylint errors

--- test.py
+++ test.py
@@ -1,22 +1,20 @@
 #!/bin/env python
-import json
-import sys
-
+"""Test docker image."""
 import docker
 
 
 client = docker.from_env()
 c = client.containers.run(
-    image='sphere',
-    command=['vv'],
+    image="sphere",
+    command=["vv"],
     stdin_open=True,
     detach=True,
 )
-s = c.attach_socket(params={'stdin': 1, 'stream': 1, 'stdout': 1, 'stderr': 1})
-x = input() + '\n'
+s = c.attach_socket(params={"stdin": 1, "stream": 1, "stdout": 1, "stderr": 1})
+x = input() + "\n"
 print(x)
-s._sock.sendall(x.encode('utf-8'))
+s._sock.sendall(x.encode("utf-8"))  # pylint: disable=protected-access
 c.wait()
-stdout = c.logs(stdout=True, stderr=False).decode('utf-8')
+stdout = c.logs(stdout=True, stderr=False).decode("utf-8")
 c.remove()
 print(stdout)
